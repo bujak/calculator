d677364d3c1026203b16fa67c7fd34b6f5a63b1a
bujak <bujakowski@gmail.com>
2016-10-29 18:27:59 +0200

    first commit

---
+++ calculator.py
@@ -0,0 +1,46 @@
+"""
+Calculator 1.0
+Tomasz Bujakowski, Codecool 2016
+"""
+
+counting = True
+
+while counting:
+
+    try:
+        num1 = int(input("Enter a number (or a letter to exit)\n"))
+    except ValueError:
+        counting = False
+        print("END")
+        break
+
+    print(str(num1) + " entered")
+
+    operator = input("Enter an operation (+, - , *, /, ^):\n")
+    while operator not in ["+", "-", "*", "/", "^"]:
+        operator = input("Enter valid operator (+, - , *, /, ^):\n")
+
+    while True:
+        try:
+            num2 = int(input("Enter second number\n"))
+            break
+        except ValueError:
+            print("Invalid number, try again")
+
+    print(str(num2) + " entered")
+
+    if operator == "+":
+        result = num1 + num2
+        print(num1, '+', num2, '=', result)
+    elif operator == "-":
+        result = num1 - num2
+        print(num1, '-', num2, '=', result)
+    elif operator == "*":
+        result = num1 * num2
+        print(num1, '*', num2, '=', result)
+    elif operator == "/":
+        result = num1 / num2
+        print(num1, '/', num2, '=', result)
+    elif operator == "^":
+        result = num1 ** num2
+        print(num1, '^', num2, '=', result)
